8619a19518c980255d7e1e421438f44a8652bcaa
Teewai57 <bobtoyoabasi@gmail.com>
2022-07-01 22:09:56 +0100

    ..

--- src/App.js
+++ src/App.js
@@ -2,6 +2,15 @@ import React, { useState, useEffect } from 'react'
 import List from './List'
 import Alert from './Alert'
 
+
+const getLocalStorage = () => {
+  let list = localStorage.getItem('list');
+  if (list) {
+    return (list = json.parse(localStorage.getItem('list')));
+  } else {
+    return [] ;
+  }
+}
 function App() {
   return <h2>grocery bud setup</h2>
 }
