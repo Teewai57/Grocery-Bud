459342cc3022d5a140a27797f35d49dd290110e6
Teewai57 <bobtoyoabasi@gmail.com>
2022-07-01 22:12:27 +0100

    returned in app js

--- src/App.js
+++ src/App.js
@@ -10,9 +10,37 @@ const getLocalStorage = () => {
   } else {
     return [] ;
   }
-}
+};
 function App() {
-  return <h2>grocery bud setup</h2>
+  return (
+    <section className='section-center'>
+      <form className='grocery-form' onSubmit={handleSubmit}>
+        {alert.show && <Alert {...alert} removeAlert={showAlert} list={list} />}
+
+        <h3>grocery bud</h3>
+        <div className='form-control'>
+          <input
+            type='text'
+            className='grocery'
+            placeholder='e.g. eggs'
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <button type='submit' className='submit-btn'>
+            {isEditing ? 'edit' : 'submit'}
+          </button>
+        </div>
+      </form>
+      {list.length > 0 && (
+        <div className='grocery-container'>
+          <List items={list} removeItem={removeItem} editItem={editItem} />
+          <button className='clear-btn' onClick={clearList}>
+            clear items
+          </button>
+        </div>
+      )}
+    </section>
+  );
 }
 
 export default App
